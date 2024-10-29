38debdd63059b45590733d4a1623661d5b1cdd6a
FASami <foysal.developer@gmail.com>
2024-10-29 20:53:54 +0600

    feat: added user registration

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,5 +1,60 @@
-const RegistrationPage = () => {
-  return <div>RegistrationPage</div>
+'use client'
+import { LockOutlined, MailOutlined } from '@ant-design/icons'
+import { AuthForm, AuthPageTitle } from '../components'
+import { RegisterSchema } from '../authSchema'
+import { registerUser } from './registerUser'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <AuthPageTitle identifier="register" />
+      <AuthForm
+        identifier="register"
+        onFormSubmit={registerUser}
+        fields={[
+          {
+            type: 'text',
+            name: 'firstName',
+            label: 'First name',
+            placeholder: 'Enter your first name',
+            initialValue: ''
+          },
+
+          {
+            type: 'text',
+            name: 'lastName',
+            label: 'Last name',
+            placeholder: 'Enter your last name',
+            initialValue: ''
+          },
+          {
+            type: 'email',
+            name: 'email',
+            label: 'Email',
+            placeholder: 'Enter your email',
+            icon: <MailOutlined />,
+            initialValue: ''
+          },
+          {
+            type: 'phone',
+            name: 'phone',
+            label: 'Phone',
+            placeholder: 'Enter your phone',
+            initialValue: ''
+          },
+          {
+            type: 'password',
+            name: 'password',
+            label: 'Password',
+            placeholder: 'Enter your password',
+            icon: <LockOutlined />,
+            initialValue: ''
+          }
+        ]}
+        schema={RegisterSchema}
+      />
+    </div>
+  )
 }
 
-export default RegistrationPage
+export default RegisterPage
---
+++ src/app/(auth)/register/registerUser.ts
@@ -0,0 +1,95 @@
+'use server'
+
+import bcrypt from 'bcryptjs'
+import { signIn } from '@/auth'
+import { RegisterSchema } from '../authSchema'
+import { AuthAction } from '@/types/authForm'
+import { getUser } from '@/app/query'
+import { apolloClient } from '@/app/lib'
+import { CREATE_USER } from '@/app/graphql/mutation'
+import {
+  InsertUsersMutation,
+  InsertUsersMutationVariables
+} from '@/types/gql/graphql'
+import { isEmpty } from 'lodash'
+import { redirect } from 'next/navigation'
+import { encrypt } from '@/app/utils/cryptography'
+
+const registerUser: AuthAction<typeof RegisterSchema> = async (
+  values,
+  { callbackUrl }
+) => {
+  let token = ''
+  try {
+    const validatedFields = RegisterSchema.safeParse(values)
+    if (!validatedFields.success) {
+      return { success: false, error: 'Invalid fields!' }
+    }
+
+    const { email, password, firstName, lastName, phone } = validatedFields.data
+    const user = await getUser({ email, phone })
+
+    if (user) {
+      if (user?.phone === phone) {
+        return { success: false, error: 'Phone number already exists!' }
+      }
+      if (user?.email === email) {
+        return { success: false, error: 'Email already exists!' }
+      }
+    }
+    const userDetails: InsertUsersMutationVariables = {
+      object: {
+        name: `${firstName} ${lastName}`,
+        ...(email && { email }),
+        ...(phone && { phone }),
+        password: await bcrypt.hash(password, 10)
+      }
+    }
+
+    const data = await apolloClient.request<InsertUsersMutation>(
+      CREATE_USER,
+      userDetails
+    )
+    if (!isEmpty(data.insert_users_one)) {
+      if (data.insert_users_one?.phone) {
+        await signIn('phone_password', {
+          phone,
+          password,
+          redirect: false
+        })
+      } else if (data.insert_users_one?.email) {
+        await signIn('email_password', {
+          email,
+          password,
+          redirect: false
+        })
+      }
+
+      token = await encrypt({
+        email: data.insert_users_one?.email,
+        phone: data.insert_users_one?.phone,
+        name: data.insert_users_one?.name,
+        scope: 'REGISTER'
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      error: 'Something went wrong !'
+    }
+  }
+  /**
+   * In Server Actions and Route Handlers, redirect should be called after the try/catch block.
+   * Token will be populated if authentication and user creating is successful and then we will redirect
+   * */
+
+  if (!isEmpty(token)) {
+    redirect(
+      `/verify?token=${encodeURIComponent(
+        token
+      )}&callbackUrl=${encodeURIComponent(callbackUrl)}`
+    )
+  }
+}
+export { registerUser }
