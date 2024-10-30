cbfc7f6ef0ccc77b83d722dcc1812456006ee8ee
FASami <foysal.developer@gmail.com>
2024-10-30 13:14:27 +0600

    fix: revalidate app on logout

--- src/app/(auth)/logout.ts
+++ src/app/(auth)/logout.ts
@@ -1,8 +1,10 @@
 'use server'
 
 import { signOut } from '@/auth'
+import { revalidatePath } from 'next/cache'
 
 const logout = async () => {
   await signOut()
+  revalidatePath('src/app')
 }
 export { logout }
