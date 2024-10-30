688ca0ae5c519be4e4826f389f1e3f5995fea391
FASami <foysal.developer@gmail.com>
2024-10-30 12:49:59 +0600

    feat: added schema for authorization

---
+++ src/app/(auth)/authSchema.ts
@@ -0,0 +1,196 @@
+import { AuthPageIdentifier } from '@/types/authForm'
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+import * as z from 'zod'
+
+export const LoginSchema = z
+  .object({
+    phone: z
+      .string()
+      .refine(
+        (value) => {
+          if (!value) return true
+          const phoneNumber = parsePhoneNumberFromString(value)
+          return phoneNumber?.isValid()
+        },
+        {
+          message: 'Invalid phone number'
+        }
+      )
+      .optional(),
+    email: z
+      .string()
+      .optional()
+      .refine(
+        (value) => !value || z.string().email().safeParse(value).success,
+        {
+          message: 'Invalid email'
+        }
+      ),
+    password: z.string().min(8, 'Password must be at least 8 characters long')
+  })
+  .superRefine((data, ctx) => {
+    if (!data.email && !data.phone) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['email']
+      })
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['phone']
+      })
+    }
+  })
+
+export const RegisterSchema = z
+  .object({
+    firstName: z.string().min(1, {
+      message: 'First name is required'
+    }),
+    lastName: z.string().min(1, {
+      message: 'Last name is required'
+    }),
+    phone: z
+      .string()
+      .refine(
+        (value) => {
+          if (!value) return true
+          const phoneNumber = parsePhoneNumberFromString(value)
+          return phoneNumber?.isValid()
+        },
+        {
+          message: 'Invalid phone number'
+        }
+      )
+      .optional(),
+    email: z
+      .string()
+      .optional()
+      .refine(
+        (value) => !value || z.string().email().safeParse(value).success,
+        {
+          message: 'Invalid email'
+        }
+      ),
+    password: z.string().min(8, 'Password must be at least 8 characters long')
+  })
+  .superRefine((data, ctx) => {
+    if (!data.email && !data.phone) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['email']
+      })
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['phone']
+      })
+    }
+  })
+
+export const VerifyOTPSchema = z
+  .object({
+    phone: z
+      .string()
+      .refine(
+        (value) => {
+          if (!value) return true
+          const phoneNumber = parsePhoneNumberFromString(value)
+          return phoneNumber?.isValid()
+        },
+        {
+          message: 'Invalid phone number'
+        }
+      )
+      .optional(),
+    email: z
+      .string()
+      .optional()
+      .refine(
+        (value) => !value || z.string().email().safeParse(value).success,
+        {
+          message: 'Invalid email'
+        }
+      ),
+    otp: z.string().length(6, {
+      message: 'OTP must be exactly 6 digits'
+    })
+  })
+  .superRefine((data, ctx) => {
+    if (!data.email && !data.phone) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['email']
+      })
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['phone']
+      })
+    }
+  })
+
+export const ForgotPasswordSchema = z
+  .object({
+    phone: z
+      .string()
+      .refine(
+        (value) => {
+          if (!value) return true
+          const phoneNumber = parsePhoneNumberFromString(value)
+          return phoneNumber?.isValid()
+        },
+        {
+          message: 'Invalid phone number'
+        }
+      )
+      .optional(),
+    email: z
+      .string()
+      .optional()
+      .refine(
+        (value) => !value || z.string().email().safeParse(value).success,
+        {
+          message: 'Invalid email'
+        }
+      )
+  })
+  .superRefine((data, ctx) => {
+    if (!data.email && !data.phone) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['email']
+      })
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Either email or phone number is required',
+        path: ['phone']
+      })
+    }
+  })
+
+export const ResetPasswordSchema = z
+  .object({
+    password: z.string().min(8, 'Password must be at least 8 characters long'),
+    confirmPassword: z.string(),
+    token: z.string()
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword']
+  })
+
+export const schemas: Record<
+  AuthPageIdentifier,
+  { schema: z.ZodType<unknown> }
+> = {
+  login: { schema: LoginSchema },
+  register: { schema: RegisterSchema },
+  forgotPassword: { schema: ForgotPasswordSchema },
+  setPassword: { schema: ResetPasswordSchema },
+  verify: { schema: VerifyOTPSchema }
+}
